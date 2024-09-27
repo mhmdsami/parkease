2738bcebb22e1e3b3947479afeed872d2f87a6d5
Mohamed Sami <srmf2025@gmail.com>
2024-09-27 19:52:59 +0530

    init: initialize app

---
+++ app/app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+export default function RootLayout() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [loaded, error] = useFonts({
+    "MonaSans-Bold": require("../assets/fonts/MonaSans-Bold.otf"),
+    "MonaSans-Medium": require("../assets/fonts/MonaSans-Medium.otf"),
+    "MonaSans-SemiBold": require("../assets/fonts/MonaSans-SemiBold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
